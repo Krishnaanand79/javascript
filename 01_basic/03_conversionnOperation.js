77ecae3e6a5bc92180b26dd3fbf987c995cd3c32
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-07-16 21:56:55 +0530

    coversion_operatio

--- 01_basic/03_conversionnOperation.js
+++ 01_basic/03_conversionnOperation.js
@@ -1,11 +1,11 @@
 let score = "Krishna"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" =>33
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true 0=> false
 // "" => false
@@ -25,6 +25,32 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  ***************** Operations *************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + 2 + "2")
+
+console.log( (3+4) * 5 % 3);
+
+
+
+
+
+
 
