8934e2f4e14e82a086fd085286f84b9932652fef
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-07-18 21:42:54 +0530

    stack and heap

---
+++ 01_basic/05_stack_heap.js
@@ -0,0 +1,22 @@
+let myyoutubename = "krishna"
+
+let anothername = myyoutubename
+anothername = "chaiaurcode"
+
+console.log(myyoutubename);
+console.log(anothername)
+
+let userOne = {
+    email :"user@google.com",
+    upi:"user@ybkl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "krish@.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
