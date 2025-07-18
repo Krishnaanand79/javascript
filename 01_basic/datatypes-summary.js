467b04465db20b3967b18fa104a2ef914e1e286c
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-07-18 21:18:18 +0530

    summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  primitive data  types 
+//  7 types  : number , Boolean , string , null , undefined , symbol , BigInt 
+
+const score = 100 ; 
+const scoreValue = 100.3
+
+const isLoggedIn  = false;
+const outsideTemp = null;
+let userEmail;
+
+
+// const id =  symbol('123')
+// const anotherId = symbol('123')
+
+
+
+
+
+/* 
+Non primitive
+Array , objects , function
+ */
+
+const heros =["shaktiman", "don ", "biods"]
+
+let myObj = {
+    name: "Krishna Anand",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+console.log(typeof myFunction );
