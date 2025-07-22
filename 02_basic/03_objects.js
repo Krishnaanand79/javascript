465776786b8b9ef7405b375b884b1b718e240511
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-07-23 03:27:08 +0530

    objects

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,37 @@
+
+const mySym = Symbol("key1")// create a symbol with description
+
+const jsUser = {
+    name: "Krishna",
+    "full name" : "krishna anand",
+    [mySym] : "mykey1", // symbol key is used as key 
+    age: 18 ,
+    location: "chandigarh",
+    email: "krishna@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays:["Monday , friday"]
+}
+
+// two ways to define objects 
+console.log(jsUser.isLoggedIn)
+// another ways are 
+console.log(jsUser["email"])
+console.log(jsUser["full name"])
+
+console.log(typeof jsUser[mySym]) // access the symbol key 
+
+jsUser.email = "krishna@.com" // change or replace the email
+// freeze - it helps us to freeze the value that cannot be changed
+// Object.freeze(jsUser)
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello js user");
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello js user ${this.name}`);
+}
+
+console.log(jsUser.greeting())
+console.log(jsUser.greetingTwo())
