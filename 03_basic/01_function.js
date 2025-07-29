cacc0ffefb3beefdb09f73da51c4656d5085650a
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-07-29 18:04:41 +0530

    expand of fun

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -22,3 +22,39 @@ function loginUserMessage(username = "krishna"){ // if you does not pass then va
 // console.log(loginUserMessage("Krishna"))
 
 console.log(loginUserMessage())// if the value  doesnot pass then its gives undefined 
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300 , 500, 1000))
+
+//object
+
+const user = {
+    username: "Krishna",
+    price: 199,
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// Pass the user object as an argument toit 
+
+//  handleObject(user)
+
+handleObject({
+    username: "Sam",
+    price : 87
+})
+
+// passing the array 
+
+const myNewArray = [200 , 400 , 100 , 600]
+
+function returnValues(getArray){
+    return getArray[1]
+}
+
+console.log(returnValues(myNewArray))
