81174bd24c4e81fb93bea5b586c52717a570b4b0
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-08-05 20:04:10 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month= 4
+switch (month) {
+    case 1:
+        console.log("janauary");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
