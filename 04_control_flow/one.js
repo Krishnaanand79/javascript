1cc211e9c67633b2695cffd2a9328b40f7e74867
Krishnaanand79 <krishnaanand1207@gmail.com>
2025-08-05 20:03:49 +0530

    if _else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less than 50 ");
+// }
+// else{
+//     console.log("temperature is greater than 50 ");
+// }
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power ="fly"
+//     console.log(`user power :${power}`);
+// }
+
+const balanace = 1000
+
+// if(balance > 500) console.log("test") , console.log("test2");
+
+
+
+// if(balanace <500){
+//     console.log("less than 500");
+// }
+// else if(balanace <750){
+//     console.log("less than 750")
+// }
+// else if (balanace <900){
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+
+const userloggedIn =true
+const debitcard = true 
+const loggedInfromGoogle =false
+const loogedInfromEmail = true
+
+if(userloggedIn && debitcard && 2==2 ){
+    console.log("Allow to buy courses");
+}
+
+if(loggedInfromGoogle || loogedInfromEmail){
+    console.log("user logged in ")
+}
+
